5576d50940cea17dc9af58b98446ba528b525f0c
weesheng.chu <weesheng10@gmail.com>
2020-03-02 18:05:31 +0800

    Updates

---
+++ precache-manifest.4814394c5bf492fbdb4fb071833fc90d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d92c7f9de7622f9f2370606d2ecd2337",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "d17d625de7947e42f8f6",
+    "url": "/react-portfolio/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "b41569501d3edbe5388b",
+    "url": "/react-portfolio/static/css/main.91a5c7d1.chunk.css"
+  },
+  {
+    "revision": "d17d625de7947e42f8f6",
+    "url": "/react-portfolio/static/js/2.e737fc69.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/react-portfolio/static/js/2.e737fc69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b41569501d3edbe5388b",
+    "url": "/react-portfolio/static/js/main.67f8a838.chunk.js"
+  },
+  {
+    "revision": "314ea9113d83e5d9f1d4",
+    "url": "/react-portfolio/static/js/runtime-main.1eb417a9.js"
+  },
+  {
+    "revision": "ee9feef44ba9e81f8672115c1395e7be",
+    "url": "/react-portfolio/static/media/Myself1.ee9feef4.jpeg"
+  },
+  {
+    "revision": "b302f5db789a36c7e5571dfffa60f6a2",
+    "url": "/react-portfolio/static/media/angular.b302f5db.png"
+  },
+  {
+    "revision": "eebb1b3317451d59966911f4b58652b7",
+    "url": "/react-portfolio/static/media/flask3.eebb1b33.png"
+  }
+]);
